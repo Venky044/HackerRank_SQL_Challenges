a9fd813e6075da9eb1d893a282078c5e3c7c692a
Venkatesh M <115688026+Venky044@users.noreply.github.com>
2024-04-15 12:43:49 +0530

    Population Density Difference

---
+++ 30-Population Density Difference.sql
@@ -0,0 +1,7 @@
+--         Population Density Difference
+
+-- Query the difference between the maximum and minimum populations in CITY.
+
+-- Solution:
+Select max(population) - min(population)
+From City;
