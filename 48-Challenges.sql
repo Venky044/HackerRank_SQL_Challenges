be5e178896d0ecb54299872ef218b67b6b4ecac8
Venkatesh M <115688026+Venky044@users.noreply.github.com>
2024-04-24 18:34:24 +0530

    Challenges

--- 48-Challenges.sql
+++ 48-Challenges.sql
@@ -1,6 +1,7 @@
         -- Challenges
 
 -- Julia asked her students to create some coding challenges. 
+
 -- Write a query to print the hacker_id, name, and the total number of challenges created by each student. 
 -- Sort your results by the total number of challenges in descending order. 
 -- If more than one student created the same number of challenges, then sort the result by hacker_id. 
@@ -32,3 +33,21 @@ where num_of_ch = (select max(num_of_ch) from sample_data) or
                     group by s.num_of_ch
                     having count(s.num_of_ch) = 1)
 order by num_of_ch desc, hacker_id;
+
+--  OR
+
+with sample_data as (
+    select hacker_id, count(hacker_id) as t_num_ch
+    from challenges 
+    group by hacker_id)
+
+select h.hacker_id, h.name, s.t_num_ch
+from hackers h
+    inner join
+    sample_data s on h.hacker_id = s.hacker_id
+where s.t_num_ch = (select max(t_num_ch) from sample_data) or
+      s.t_num_ch in (select t_num_ch from sample_data
+                    group by t_num_ch
+                    having count(t_num_ch) = 1)
+group by h.hacker_id, h.name
+order by s.t_num_ch desc, h.hacker_id;
